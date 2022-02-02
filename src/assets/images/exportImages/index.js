ba13108f724a9e695c8724446aa11b5809cdf9c3
ourainbows <casasjuanfelipe@gmail.com>
2022-02-02 13:03:49 -0500

    Import images from imagesExport in data.js

---
+++ src/assets/images/exportImages/index.js
@@ -0,0 +1,15 @@
+import brassier from "../brassier.jpg"
+import bufanda from "../bufanda.jpg"
+import bufandaInfinita from "../bufandaInfinita.jpg"
+import bustier from "../bustier.jpg"
+import conjuntoMujer from "../conjuntoMujer.jpg"
+import sacoMascota from "../sacoMascota.jpg"
+
+export {
+    brassier,
+    bufanda,
+    bufandaInfinita,
+    bustier,
+    conjuntoMujer,
+    sacoMascota,
+}
--- src/data.js
+++ src/data.js
@@ -1,75 +1,77 @@
+import {brassier,bufanda,bufandaInfinita,bustier,conjuntoMujer,sacoMascota} from "./assets/images/exportImages"
+
 const dataProdcuts = [
     {
         id: 1,
         name: "Bustier",
         colors: ["#5A0912", "#BF8B38", "#0F858C"],
         size: ["S"],
-        price: 35.000,
+        price: 35000,
         category: 1,
-        image: "url"
+        image: bustier
     },
     {
         id: 2,
         name: "Saco para mascota",
         colors: ["#C0478E", "#120820", "#C1B8B6"],
         size: ["U"],
-        price: 20.000,
+        price: 20000,
         category: 2,
-        image: "url "
+        image: sacoMascota
     },
     {
         id: 3,
         name: "Bufanda Infinita",
         colors: ["#CE8F30", "#405459"],
         size: ["U"],
-        price: 25.000,
+        price: 25000,
         category: 1,
-        image: "url "
+        image: bufandaInfinita
     },
     {
         id: 4,
         name: "Conjunto ropa Interior",
         colors: ["#5A85A5", "#B3BDC6", "#BBBFC0"],
         size: ["U", "S", "SX "],
-        price: 45.000,
+        price: 45000,
         category: 1,
-        image: "url "
+        image: conjuntoMujer
     },
     {
         id: 5,
         name: "Bufanda",
         colors: ["#306764"],
         size: ["U"],
-        price: 20.000,
+        price: 20000,
         category: 1,
-        image: "url "
+        image: bufanda
     },
     {
         id: 6,
         name: "Gorro para mascota",
         colors: ["#608BAC"],
         size: ["U"],
-        price: 10.000,
+        price: 10000,
         category: 2,
-        image: "url "
+        image: sacoMascota
     },
     {
         id: 7,
         name: "Conjunto para mascota ",
         colors: ["#955E6B", "#A6A7A2"],
         size: ["U"],
-        price: 25.000,
+        price: 25000,
         category: 2,
-        image: "url "
+        image: sacoMascota
     },
     {
         id: 8,
         name: "Brassier",
         colors: ["#CDA0B5", "#80A9A3"],
         size: ["U", "S", "L"],
-        price: 20.000,
+        price: 20000,
         category: 1,
-        image: "url "
+        image: brassier
     }
 ]
 
