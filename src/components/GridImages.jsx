65d517ad518f2a9c475e884c7d06e8dbc71836a2
ourainbows <casasjuanfelipe@gmail.com>
2022-02-03 12:13:11 -0500

    Basic display of grid images

--- src/components/GridImages.jsx
+++ src/components/GridImages.jsx
@@ -1,17 +1,30 @@
 import styles from "../styles/gridImages.module.css"
-import {scarfImg} from "../assets/images/bufanda.jpg"
-import {bustierImg} from "../assets/images/bustier.jpg"
-import {infiniteScarfImg} from "../assets/images/bufandaInfinita.jpg"
-import {brassierImg} from "../assets/images/brassier.jpg"
+
+import scarfImg from "../assets/images/bufanda.jpg"
+import bustierImg from "../assets/images/bustier.jpg"
+import infiniteScarfImg from "../assets/images/bufandaInfinita.jpg"
+import brassierImg from "../assets/images/brassier.jpg"
 
 
 function GridImages() {
-    return ( 
+    return (
         <div className={styles.containerGrid}>
-            <img src={scarfImg} alt="Modelo con prenda hecha en croshet"/>
-            <img src={ bustierImg} alt="Modelo con prenda hecha en croshet"/>
-            <img src={infiniteScarfImg } alt="Modelo con prenda hecha en croshet"/>
-            <img src={brassierImg } alt="Modelo con prenda hecha en croshet"/>
+            <img
+                className={`${styles.gridImage1} ${styles.gridImage}`}
+                src={scarfImg}
+                alt="Modelo con prenda hecha en croshet" />
+            <img
+                className={`${styles.gridImage2} ${styles.gridImage}`}
+                src={bustierImg}
+                alt="Modelo con prenda hecha en croshet" />
+            <img
+                className={`${styles.gridImage3} ${styles.gridImage}`}
+                src={infiniteScarfImg}
+                alt="Modelo con prenda hecha en croshet" />
+            <img
+                className={`${styles.gridImage4} ${styles.gridImage}`}
+                src={brassierImg}
+                alt="Modelo con prenda hecha en croshet" />
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import bgCoat from "../assets/images/bgCoat.jpg"
 import { ButtonProdcuts } from "../components/ButtonProducts"
+import { GridImages } from "../components/GridImages"
 import styles from "../styles/home.module.css"
 
 function Home() {
@@ -15,7 +16,7 @@ function Home() {
             <p className={styles.aboutStore}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit exercitationem ipsum voluptatem est, aliquam totam modi non, error atque autem, dolor ut quaerat deleniti. Et harum illo ducimus porro iusto.
                 Quo eligendi fugit ut in neque provident libero quos, maiores fuga voluptate qui iste minima doloribus natus pariatur delectus sapiente aperiam. Quo hic magnam dolores, et nobis ratione quasi quidem?</p>
             <ButtonProdcuts/>
-            
+            <GridImages/>
         </main>
     )
 }
