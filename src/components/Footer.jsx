099f16832d8eb76e9d67db81e9446b7eb62b161a
ourainbows <casasjuanfelipe@gmail.com>
2022-02-07 14:31:48 -0500

    Changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,9 @@ function Footer() {
             <div className={styles.containerLogoFooter}>
                 <img src={logo} alt="Logo de la empresa"/>
                 <span>Amaranta</span>
+                <a href="https://www.tiktok.com/@lacatt99">@lacatt99</a>
             </div>
+            <span className={styles.creator}>Powered by Juan Casas</span>
         </footer>
     )
 }
