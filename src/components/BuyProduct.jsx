edaf5edb3d49284c97d42fcfc106e54a326043af
ourainbows <casasjuanfelipe@gmail.com>
2022-02-06 23:01:47 -0500

    Created form than contains user name, address and comments

---
+++ src/components/BuyProduct.jsx
@@ -0,0 +1,8 @@
+import styles from "../styles/buyProduct.module.css"
+
+function BuyProduct() {
+    return (
+        <div>Hola</div>
+    )
+}
+export {BuyProduct}
--- src/pages/Buy.jsx
+++ src/pages/Buy.jsx
@@ -1,7 +1,44 @@
+import { useState } from "react"
+import { BuyProduct } from "../components/BuyProduct"
+/* import styles from "../styles/buy.module.css" */
+
 function Buy() {
+    const [inputs, setInputs] = useState({})
+    const handleChange = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+        setInputs(prevState => ({ ...prevState, [name]: value }))
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(inputs)
+    }
+
     return (
-        <div style={{ marginTop: "100px" }}>Vista de compras</div>
+        <main style={{ marginTop: "100px" }}>
+            <h2>Tu orden</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    name="user"
+                    type="text"
+                    placeholder="Nombres y Apellidos"
+                    onChange={handleChange}
+                />
+                <input
+                    name="address"
+                    type="text"
+                    placeholder="Direccion"
+                    onChange={handleChange}
+                />
+                <textarea
+                    name="comments"
+                    placeholder="Comentarios o requerimientos adicionales"
+                    onChange={handleChange}
+                />
+                <input type="submit" />
+            </form>
+        </main>
     )
 }
 
-export {Buy}
+export { Buy }
