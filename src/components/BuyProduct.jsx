e3a29b855e479b51404d0229378e5c74cdddad75
ourainbows <casasjuanfelipe@gmail.com>
2022-02-07 00:51:09 -0500

    Functionality and styles to buy view

--- src/components/BuyProduct.jsx
+++ src/components/BuyProduct.jsx
@@ -1,8 +1,24 @@
 import styles from "../styles/buyProduct.module.css"
 
-function BuyProduct() {
+function BuyProduct({ product }) {
     return (
-        <div>Hola</div>
+        <>
+        <div className={styles.productForm}>
+            <img src={product.image} alt={product.name}/>
+            <div className={styles.productInfo}>
+                <h3 className={styles.productFormName}>{product.name}</h3>
+                <span className={styles.productFormPrice}>{product.price.toFixed(3)}</span>
+                <div className={styles.productFormValues}>
+                    <span className={styles.productFormValuesQuantity}>Cantidad: {product.quantity}</span>
+                    <span className={styles.productFormValuesColor}>Color:
+                            <div className={styles.valuesColor} style={{ backgroundColor: product.color }}></div>
+                    </span>
+                    <span className={styles.productFormValuesHeigth}>Talla { product.height}</span>
+                </div>
+            </div>
+        </div>
+            <hr className={styles.productDivider} />
+        </>
     )
 }
-export {BuyProduct}
+export { BuyProduct }
--- src/pages/Buy.jsx
+++ src/pages/Buy.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import { BuyProduct } from "../components/BuyProduct"
-/* import styles from "../styles/buy.module.css" */
+import { ProductsContext } from "../context/ProductsProvider/ProductsProvider"
+import styles from "../styles/buy.module.css"
 
 function Buy() {
     const [inputs, setInputs] = useState({})
+    const { cartProducts, totalPrice } = useContext(ProductsContext)
     const handleChange = (event) => {
         const name = event.target.name
         const value = event.target.value
@@ -11,13 +13,12 @@ function Buy() {
     }
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(inputs)
     }
 
     return (
-        <main style={{ marginTop: "100px" }}>
+        <main className={styles.mainBuy}>
             <h2>Tu orden</h2>
-            <form onSubmit={handleSubmit}>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <input
                     name="user"
                     type="text"
@@ -35,8 +36,20 @@ function Buy() {
                     placeholder="Comentarios o requerimientos adicionales"
                     onChange={handleChange}
                 />
-                <input type="submit" />
+                {/* <input type="submit" /> */}
             </form>
+            <div className={styles.productsForm}>
+                {cartProducts.map((product, i) => (
+                    <BuyProduct product={product} key={i} />
+                ))}
+            </div>
+            <hr className={styles.bigDvider}/>
+            <div className={styles.price}>
+                <span className={styles.priceText}>Total</span>
+                <span className={styles.priceNumber}>{ totalPrice.toFixed(3)}</span>
+            </div>
+            <div className={styles.btnBuy}><a href="https://wa.me/573156595651?text=Comprar Productos">COMPRAR</a></div>
+            <p className={styles.pInfo}>* Serás redireccionado a Whatsapp para terminar tu compra</p>
         </main>
     )
 }
