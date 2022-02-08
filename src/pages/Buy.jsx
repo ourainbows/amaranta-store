3b72900c36e265e772efaadb298b68f507f108b4
ourainbows <casasjuanfelipe@gmail.com>
2022-02-08 10:31:52 -0500

    Responsive for buy

--- src/pages/Buy.jsx
+++ src/pages/Buy.jsx
@@ -7,7 +7,7 @@ function Buy() {
     const [inputs, setInputs] = useState({})
     const { cartProducts, totalPrice } = useContext(ProductsContext)
     const [url, setUrl] = useState("")
-    
+
     const message = () => {
         let text = "https://wa.me/573156595651?text="
         text += `Buenas tardes mi nombre es ${inputs.user ? inputs.user : ""}, te contacto desde la tienda en linea Amaranta ya que estoy interesad@ en los siguientes productos: \n\n`
@@ -30,42 +30,44 @@ function Buy() {
         event.preventDefault();
     }
     return (
-        <main className={styles.mainBuy}>
-            <h2>Tu orden</h2>
-            <form className={styles.form} onSubmit={handleSubmit}>
-                <input
-                    name="user"
-                    type="text"
-                    placeholder="Nombres y Apellidos"
-                    onChange={handleChange}
-                />
-                <input
-                    name="address"
-                    type="text"
-                    placeholder="Dirección"
-                    onChange={handleChange}
-                />
-                <textarea
-                    name="comments"
-                    placeholder="Comentarios o requerimientos adicionales"
-                    onChange={handleChange}
-                />
-                {/* <input type="submit" /> */}
-            </form>
-            <div className={styles.productsForm}>
-                {cartProducts.map((product, i) => (
-                    <BuyProduct product={product} key={i} />
-                ))}
+        <main className={styles.containerBuy}>
+            <div className={styles.mainBuy}>
+                <h2>Tu orden</h2>
+                <form className={styles.form} onSubmit={handleSubmit}>
+                    <input
+                        name="user"
+                        type="text"
+                        placeholder="Nombres y Apellidos"
+                        onChange={handleChange}
+                    />
+                    <input
+                        name="address"
+                        type="text"
+                        placeholder="Dirección"
+                        onChange={handleChange}
+                    />
+                    <textarea
+                        name="comments"
+                        placeholder="Comentarios o requerimientos adicionales"
+                        onChange={handleChange}
+                    />
+                    {/* <input type="submit" /> */}
+                </form>
+                <div className={styles.productsForm}>
+                    {cartProducts.map((product, i) => (
+                        <BuyProduct product={product} key={i} />
+                    ))}
+                </div>
+                <hr className={styles.bigDvider} />
+                <div className={styles.price}>
+                    <span className={styles.priceText}>Total</span>
+                    <span className={styles.priceNumber}>{totalPrice.toFixed(3)}</span>
+                </div>
+                <div className={styles.btnBuy}>
+                    <a href={url} onClick={message} target="_blank" rel="noreferrer">COMPRAR</a>
+                </div>
+                <p className={styles.pInfo}>* Serás redireccionado a Whatsapp para terminar tu compra</p>
             </div>
-            <hr className={styles.bigDvider} />
-            <div className={styles.price}>
-                <span className={styles.priceText}>Total</span>
-                <span className={styles.priceNumber}>{totalPrice.toFixed(3)}</span>
-            </div>
-            <div className={styles.btnBuy}>
-                <a href={url} onClick={message} target="_blank" rel="noreferrer">COMPRAR</a>
-            </div>
-            <p className={styles.pInfo}>* Serás redireccionado a Whatsapp para terminar tu compra</p>
         </main>
     )
 }
